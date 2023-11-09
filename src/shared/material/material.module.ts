704f6fe55de91692c6a184a6c6e5af9dc18a4504
Hrushikesh <hrushikesh.surkar@greatplaceitservices.com>
2023-11-09 22:26:21 +0530

    angular material update and some modificaiton in loign page

--- src/shared/material/material.module.ts
+++ src/shared/material/material.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
-
-
+const MaterialExport = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatCardModule
+]
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ...MaterialExport,
+  ],
+  exports: [
+    ...MaterialExport,
   ]
 })
+
 export class MaterialModule { }
